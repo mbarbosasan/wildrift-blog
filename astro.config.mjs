93371d7dc469b0c7336d4f1971aabe293bbc6dae
Moises <moisesbarbosa23@gmail.com>
2024-04-13 02:05:03 -0300

    feat: scaffold inicial c/ tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), icon(), react(), tailwind()]
+});

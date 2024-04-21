50ab3cf981ce70d0e3c05110e892e68e04ed1f35
Moises <moisesbarbosa23@gmail.com>
2024-04-21 13:51:38 -0300

    feat[consts]: alterando titulo e descrição da página.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Celulol Blog';
+export const SITE_DESCRIPTION = 'Apenas um blog sobre o League of Legends, ou o celulol!';
